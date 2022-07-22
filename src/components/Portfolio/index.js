6828face90bcdcba859ee5f8be8b20615440eee7
Emma Olson <emma.olson794@gmail.com>
2022-07-21 20:34:18 -0500

    restructuring projects array

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,10 +3,39 @@ import Project from '../Project';
 
 function Portfolio() {
 
+    const projects = [
+        {
+            title: 'Swappers Meet',
+            file_name: 'swappers-meet.png',
+            github: 'MNlegion/Swappers-Meet',
+            deployment: 'https://swappers-meet.herokuapp.com/'
+        },
+        {
+            title: 'Trip Consultant',
+            file_name: 'flight-tracker-app.png',
+            github: 'emmazart/bootcamp-project-1',
+            deployment: 'https://emmazart.github.io/bootcamp-project-1/'
+        },
+        {
+            title: 'Coding Quiz',
+            file_name: 'coding-quiz-gif.gif',
+            github: 'emmazart/coding-quiz',
+            deployment: 'https://emmazart.github.io/coding-quiz/'
+        },
+        {
+            title: 'Untitled Project (Coming Soon)',
+            file_name: 'coming-soon.png',
+            github: 'emmazart',
+            deployment: 'https://github.com/emmazart'
+        }
+    ];
+
     return(
         <div id='portfolio'>
             <h2 className='test'>Portfolio</h2>
-            <Project />
+            {projects.map(project => 
+                {<Project title={project.title} fileName={project.file_name} github={project.github} deployment={project.deployment} />}
+            )}
         </div>
     )
 };
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,59 +2,54 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-const projects = [
-    {
-        title: 'Swappers Meet',
-        file_name: 'swappers-meet.png',
-        github: 'MNlegion/Swappers-Meet',
-        deployment: 'https://swappers-meet.herokuapp.com/'
-    },
-    {
-        title: 'Trip Consultant',
-        file_name: 'flight-tracker-app.png',
-        github: 'emmazart/bootcamp-project-1',
-        deployment: 'https://emmazart.github.io/bootcamp-project-1/'
-    },
-    {
-        title: 'Coding Quiz',
-        file_name: 'coding-quiz-gif.gif',
-        github: 'emmazart/coding-quiz',
-        deployment: 'https://emmazart.github.io/coding-quiz/'
-    },
-    {
-        title: 'Untitled Project (Coming Soon)',
-        file_name: 'coming-soon.png',
-        github: 'emmazart',
-        deployment: 'https://github.com/emmazart'
-    }
-];
+// function Project() {
 
-function Project() {
+//     return(
+//         <section>
+//             {projects.map(project => {
 
-    return(
-        <section>
-            {projects.map(project => {
+//                 return(
+//                     <div className='project-card'>
+//                         <h3>{`${project.title}`}</h3>
+//                         <img src={require(`../../assets/app_images/${project.file_name}`)} style={{width: "400px"}}></img>
+//                         <p>
+//                             <a href={`${project.deployment}`}>
+//                                 View deployment
+//                             </a>
+//                         </p>
+//                         <p>
+//                             <a href={`https://github.com/${project.github}`}>
+//                                 Check out {`${project.title}`} on Github <FontAwesomeIcon icon={faGithub}/>
+//                             </a>
+//                         </p>
+//                     </div>
+//                 )
+            
+//             })}
+//         </section>
+//     )
+// }
+
+
+function Project(props) {
+    const { title, fileName, github, deployment } = props;
 
                 return(
                     <div className='project-card'>
-                        <h3>{`${project.title}`}</h3>
-                        <img src={require(`../../assets/app_images/${project.file_name}`)} style={{width: "400px"}}></img>
+                        <h3>{`${title}`}</h3>
+                        <img src={require(`../../assets/app_images/${fileName}`)} style={{width: "400px"}}></img>
                         <p>
-                            <a href={`${project.deployment}`}>
+                            <a href={`${deployment}`}>
                                 View deployment
                             </a>
                         </p>
                         <p>
-                            <a href={`https://github.com/${project.github}`}>
-                                Check out {`${project.title}`} on Github <FontAwesomeIcon icon={faGithub}/>
+                            <a href={`https://github.com/${github}`}>
+                                Check out {`${title}`} on Github <FontAwesomeIcon icon={faGithub}/>
                             </a>
                         </p>
                     </div>
                 )
-            
-            })}
-        </section>
-    )
 }
 
 
