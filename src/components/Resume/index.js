478aa9d4739b6ce2364a74dbc5739f398077d908
Emma Olson <emma.olson794@gmail.com>
2022-07-22 15:24:15 -0500

    resume pdf embedded

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,6 +5,19 @@ function Resume() {
     return(
         <section id='resume' className="content py-3 my-2">
             <h2 className="content-title text-2xl">Resume</h2>
+            <div className='w-full h-96'>
+                <embed
+                    className='w-full h-96' 
+                    src={require("../../assets/resume.pdf")} 
+                    type="application/pdf">
+                </embed>
+            </div>
+            <p className='p-1 mt-3'>click 
+                <a className='px-1 text-tertiary' href={require("../../assets/resume.pdf")}>
+                    here
+                </a>                
+                to download
+            </p>
         </section>
     )
 }
