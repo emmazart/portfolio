03be1cc69327d8838214abdc5821f5ff25961c43
Emma Olson <emma.olson794@gmail.com>
2022-07-22 14:52:08 -0500

    card styles added to projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -25,7 +25,7 @@ const projects = [
         deployment: 'https://emmazart.github.io/coding-quiz/'
     },
     {
-        title: 'Untitled Project (Coming Soon)',
+        title: 'Untitled Project',
         file_name: 'coming-soon.png',
         alt: "colorful digital art reads 'coming soon'",
         github: 'emmazart',
@@ -40,19 +40,26 @@ function Project() {
             {projects.map(project => {
 
                 return(
-                    <div className='project-card'>
-                        <h3>{`${project.title}`}</h3>
-                        <img src={require(`../../assets/app_images/${project.file_name}`)} style={{width: "400px"}} alt={`${project.alt}`}></img>
-                        <p>
-                            <a href={`${project.deployment}`}>
-                                View deployment
-                            </a>
-                        </p>
-                        <p>
-                            <a href={`https://github.com/${project.github}`}>
-                                Check out {`${project.title}`} on Github <FontAwesomeIcon icon={faGithub}/>
-                            </a>
-                        </p>
+                    <div className='project-card flex justify-center my-8'>
+                        <div className='rounded-lg shadow-lg bg-tangerine max-w-sm'>
+                            <img 
+                                className='rounded-t-lg'
+                                src={require(`../../assets/app_images/${project.file_name}`)} style={{width: "400px"}} alt={`${project.alt}`}>
+                            </img>
+                            <div className='p-6'>
+                                <h3 className='text-xl font-medium mb-2'>{`${project.title}`}</h3>
+                                <p className='text-base'>
+                                    <a href={`${project.deployment}`}>
+                                        View deployment
+                                    </a>
+                                </p>
+                                <p className='text-base'>
+                                    <a href={`https://github.com/${project.github}`}>
+                                        Check out {`${project.title}`} on Github <FontAwesomeIcon icon={faGithub}/>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 )
             
@@ -60,6 +67,7 @@ function Project() {
         </section>
     )
 }
+
 
 
 export default Project;
